b5a3208f297219af5942a3a0bb9132c40e5aae10
ArnauTajahuerce <arnau.tajahuerce@estudiantat.upc.edu>
2024-05-07 12:52:58 +0200

    Funció getAgentInfo que de moment no s'utilitza

--- shop/AgentUtil/ACLMessages.py
+++ shop/AgentUtil/ACLMessages.py
@@ -18,9 +18,7 @@ from rdflib.namespace import RDF, FOAF
 from AgentUtil.OntoNamespaces import ACL, DSO
 from AgentUtil.OntoNamespaces import ECSDI
 from rdflib import XSD
-
-agn = Namespace("http://www.agentes.org#")
-from shop.AgentUtil import DSO
+from AgentUtil.Agent import Agent
 
 agn = Namespace("http://www.agentes.org#")
 
@@ -119,3 +117,25 @@ def registerAgent(agent, directoryAgent, typeOfAgent, messageCount):
         build_message(gmess, perf=ACL.request, sender=agent.uri, receiver=directoryAgent.uri, content=reg_obj, msgcnt=messageCount),
         directoryAgent.address
     )
+    
+def getAgentInfo(agentType, directoryAgent, sender, messageCount):
+    gmess = Graph()
+    gmess.bind('foaf', FOAF)
+    gmess.bind('dso', DSO)
+    ask_obj = agn[sender.name + '-Search']
+
+    gmess.add((ask_obj, RDF.type, DSO.Search))
+    gmess.add((ask_obj, DSO.AgentType, agentType))
+    gr = send_message(
+        build_message(gmess, perf=ACL.request, sender=sender.uri, receiver=directoryAgent.uri, msgcnt=messageCount,
+                      content=ask_obj),
+        directoryAgent.address
+    )
+    dic = get_message_properties(gr)
+    content = dic['content']
+
+    address = gr.value(subject=content, predicate=DSO.Address)
+    url = gr.value(subject=content, predicate=DSO.Uri)
+    name = gr.value(subject=content, predicate=FOAF.name)
+
+    return Agent(name, url, address, None)
